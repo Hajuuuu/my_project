232729456b0e11d46e8fd48ba0fecf0a6923b678
Kacper Cieślik <nowackipawel970@gmail.com>
2024-12-04 16:10:54 +0000

    projekt z zajec

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,22 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+#[ic_cdk::update]
+fn add_msg(new_msg: String) {
+    ic_cdk::println!("{}", new_msg);
+    CHAT.with(|chat| {
+        chat.borrow_mut().push(new_msg)
+    })
+}
+
+#[ic_cdk::query]
+fn get_chat() -> Vec<String> {
+    CHAT.with(|chat| chat.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
